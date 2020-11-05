7ce95e9417be91b15a2956b91949d7d46f642cc4
Yotam Gingold <yotam@yotamgingold.com>
2020-11-05 02:23:25 -0500

    WIP: animation and kinematics

--- src/animation.h
+++ src/animation.h
@@ -18,13 +18,16 @@ struct TRS {
     // Identity by default.
     TRS() : translation(0,0,0), rotation(0,0,0), scale(1,1,1) {}
     
-    // Convert to a mat4 as needed.
+    // Convert to a mat4 automatically as needed.
     operator mat4();
 };
 // A Pose as an array of transformations in TRS form.
 typedef std::vector< TRS > TRSPose;
 // Convert from a TRSPose to a MatrixPose.
-MatrixPose MatrixPoseFromTRSPose( const TRSPose& pose );
+MatrixPose MatrixPoseFromTRSPose( const TRSPose& pose ) {
+    MatrixPose result; result.reserve( pose.size() );
+    for( const auto& trs : pose ) result.push_back( trs );
+}
 
 struct BoneAnimation {
     // A bone animation stores a sequence of Poses.
---
+++ src/kinematics.cpp
@@ -0,0 +1,1 @@
+#include "kinematics.h"
